3a45b34194c9a9fcf8e94e76384d5fd03f55739a
66 Maintainer <66maintainer@gmail.com>
2014-03-01 00:37:21 +0200

    add Kimoto Gravity Well support

--- src/main.cpp
+++ src/main.cpp
@@ -56,12 +56,16 @@ CScript COINBASE_FLAGS;
 
 const string strMessageMagic = "66 Signed Message:\n";
 
+const int FIRST_HARDFORK_BLOCK_HEIGHT = 32000;
+
 double dHashesPerSec;
 int64 nHPSTimerStart;
 
 // Settings
 int64 nTransactionFee = 1;
 int64 nMinimumInputValue = CENT / 1000000;
+
+unsigned int static KimotoGravityWell(const CBlockIndex *pindexLast, const CBlock *pblock, uint64 TargetBlocksSpacingSeconds, uint64 PastBlocksMin, uint64 PastBlocksMax);
 
 //////////////////////////////////////////////////////////////////////////////
 //
@@ -772,10 +776,10 @@ int64 static GetBlockValue(int nHeight, int64 nFees, uint256 prevHash)
 		nSubsidy = 3 * COIN;
 	} else if (nHeight < 667) {
 		nSubsidy = 0.00000066 * COIN;
-	} else if(nHeight > 30000) {
+	} else if ((nHeight >= FIRST_HARDFORK_BLOCK_HEIGHT) || fTestNet) {
 		std::string hashhex = prevHash.ToString();
 		size_t n = std::count(hashhex.begin(), hashhex.end(), '6');
-		if( n % 6 == 0 )
+		if (n % 6 == 0)
 			nSubsidy *= 6.6;
 	}
 
@@ -822,7 +826,7 @@ unsigned int ComputeMinWork(unsigned int nBase, int64 nTime)
 	return bnResult.GetCompact();
 }
 
-unsigned int static GetNextWorkRequired(const CBlockIndex *pindexLast, const CBlock *pblock)
+unsigned int static GetNextWorkRequiredBeforeKimoto(const CBlockIndex *pindexLast, const CBlock *pblock)
 {
 	unsigned int nProofOfWorkLimit = bnProofOfWorkLimit.GetCompact();
 
@@ -908,6 +912,26 @@ unsigned int static GetNextWorkRequired(const CBlockIndex *pindexLast, const CBl
 		bnNew = bnProofOfWorkLimit;
 
 	return bnNew.GetCompact();
+}
+
+unsigned int static GetNextWorkRequiredKimoto(const CBlockIndex *pindexLast, const CBlock *pblock)
+{
+	static const int64 BlocksTargetSpacing = 66;
+	unsigned int TimeDaySeconds = 60 * 60 * 24;
+	int64 PastSecondsMin = TimeDaySeconds * 0.55;
+	int64 PastSecondsMax = TimeDaySeconds * 1.89;
+	uint64 PastBlocksMin = PastSecondsMin / BlocksTargetSpacing;
+	uint64 PastBlocksMax = PastSecondsMax / BlocksTargetSpacing;
+
+	return KimotoGravityWell(pindexLast, pblock, BlocksTargetSpacing, PastBlocksMin, PastBlocksMax);
+}
+
+unsigned int static GetNextWorkRequired(const CBlockIndex *pindexLast, const CBlock *pblock)
+{
+	if ((pindexLast->nHeight > FIRST_HARDFORK_BLOCK_HEIGHT) || fTestNet)
+		return GetNextWorkRequiredKimoto(pindexLast, pblock);
+	else
+		return GetNextWorkRequiredBeforeKimoto(pindexLast, pblock);
 }
 
 bool CheckProofOfWork(uint256 hash, unsigned int nBits)
@@ -1861,7 +1885,7 @@ bool LoadBlockIndex(bool fAllowNew)
 		pchMessageStart[1] = 0xc1;
 		pchMessageStart[2] = 0xb7;
 		pchMessageStart[3] = 0xdc;
-		hashGenesisBlock = uint256("0x2ab4aaf9a0c6a2aa540c04115fe5c6ab16d0056c0d93e643f4cb8e9923c0da60");
+		hashGenesisBlock = uint256("0x505c40ef0f4efad59c050d4f0e13cede8b454ab18ac98f2414b92f088d52705f");
 	}
 
 	//
@@ -3510,4 +3534,82 @@ void GenerateBitcoins(bool fGenerate, CWallet *pwallet)
 			Sleep(10);
 		}
 	}
+}
+
+unsigned int static KimotoGravityWell(const CBlockIndex *pindexLast, const CBlock *pblock, uint64 TargetBlocksSpacingSeconds, uint64 PastBlocksMin, uint64 PastBlocksMax)
+{
+	/* current difficulty formula, megacoin - kimoto gravity well */
+	const CBlockIndex *BlockLastSolved = pindexLast;
+	const CBlockIndex *BlockReading = pindexLast;
+	const CBlock *BlockCreating = pblock;
+	BlockCreating = BlockCreating;
+	uint64 PastBlocksMass = 0;
+	int64 PastRateActualSeconds = 0;
+	int64 PastRateTargetSeconds = 0;
+	double PastRateAdjustmentRatio = double(1);
+	CBigNum PastDifficultyAverage;
+	CBigNum PastDifficultyAveragePrev;
+	double EventHorizonDeviation;
+	double EventHorizonDeviationFast;
+	double EventHorizonDeviationSlow;
+
+	if (BlockLastSolved == NULL || BlockLastSolved->nHeight == 0 || (uint64)BlockLastSolved->nHeight < PastBlocksMin) {
+		return bnProofOfWorkLimit.GetCompact();
+	}
+
+	for (unsigned int i = 1; BlockReading && BlockReading->nHeight > 0; i++) {
+		if (PastBlocksMax > 0 && i > PastBlocksMax) {
+			break;
+		}
+		PastBlocksMass++;
+
+		if (i == 1) {
+			PastDifficultyAverage.SetCompact(BlockReading->nBits);
+		} else {
+			PastDifficultyAverage = ((CBigNum().SetCompact(BlockReading->nBits) - PastDifficultyAveragePrev) / i) + PastDifficultyAveragePrev;
+		}
+		PastDifficultyAveragePrev = PastDifficultyAverage;
+
+		PastRateActualSeconds = BlockLastSolved->GetBlockTime() - BlockReading->GetBlockTime();
+		PastRateTargetSeconds = TargetBlocksSpacingSeconds * PastBlocksMass;
+		PastRateAdjustmentRatio = double(1);
+		if (PastRateActualSeconds < 0) {
+			PastRateActualSeconds = 0;
+		}
+		if (PastRateActualSeconds != 0 && PastRateTargetSeconds != 0) {
+			PastRateAdjustmentRatio = double(PastRateTargetSeconds) / double(PastRateActualSeconds);
+		}
+		EventHorizonDeviation = 1 + (0.7084 * pow((double(PastBlocksMass) / double(215)), -1.228));
+		EventHorizonDeviationFast = EventHorizonDeviation;
+		EventHorizonDeviationSlow = 1 / EventHorizonDeviation;
+
+		if (PastBlocksMass >= PastBlocksMin) {
+			if ((PastRateAdjustmentRatio <= EventHorizonDeviationSlow) || (PastRateAdjustmentRatio >= EventHorizonDeviationFast)) {
+				assert(BlockReading);
+				break;
+			}
+		}
+		if (BlockReading->pprev == NULL) {
+			assert(BlockReading);
+			break;
+		}
+		BlockReading = BlockReading->pprev;
+	}
+
+	CBigNum bnNew(PastDifficultyAverage);
+	if (PastRateActualSeconds != 0 && PastRateTargetSeconds != 0) {
+		bnNew *= PastRateActualSeconds;
+		bnNew /= PastRateTargetSeconds;
+	}
+	if (bnNew > bnProofOfWorkLimit) {
+		bnNew = bnProofOfWorkLimit;
+	}
+
+	/// debug print
+	printf("Difficulty Retarget - Kimoto Gravity Well\n");
+	printf("PastRateAdjustmentRatio = %g\n", PastRateAdjustmentRatio);
+	printf("Before: %08x  %s\n", BlockLastSolved->nBits, CBigNum().SetCompact(BlockLastSolved->nBits).getuint256().ToString().c_str());
+	printf("After:  %08x  %s\n", bnNew.GetCompact(), bnNew.getuint256().ToString().c_str());
+
+	return bnNew.GetCompact();
 }
